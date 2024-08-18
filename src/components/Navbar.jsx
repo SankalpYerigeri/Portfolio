05159f9a50a9ad2becc724f941b9ca5540d0c526
sankalp <sankalpyerigeri20@gmail.com>
2024-08-18 20:44:14 +0530

    Updating navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,59 @@
-import React, {useState, useEffect} from 'react';
-import { Link } from 'react-router-dom';
-import { styles } from '../style';
-import { navLinks } from '../constants';
-import { logo, menu, close } from '../assets';
-
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { styles } from "../style";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
-  const [active, setActive]=useState("");
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
   return (
-    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
-      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
-        <Link to="/" className='flex items-center gap-2'
-        onClick={()=>
-        {
-          setActive("");
-          window.scroll(0,0);
-        }
-        }>
-          <img src={logo} className='w-9 h-9 object-contain'/>
-          <p className='text-white text-[18px] font-bold cursor-pointer' >Sankalp | <span className='sm: block hidden'>Associate Software Engineer</span></p>
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scroll(0, 0);
+          }}
+        >
+          <img src={logo} className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            Sankalp
+            <span className="sm: block">Associate Software Engineer</span>
+          </p>
         </Link>
-      </div>
-      
-    </nav>
-  )
-}
+        <ul className="list-none hideen sm:flex flex-row gap-3">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              } hover:text-white font-medium text-[18px] cursor-pointer`}
+              onClick={()=>setActive(link.title)}
+            >
+              <Link to={`#${link.id}`}>{link.title}</Link>
+            </li>
+          ))}
+        </ul>
 
-export default Navbar
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img src={toggle ? menu : close} alt="menu"
+           className="w-[28px] h-[28px] object-contain cursor-pointer"
+           onClick={()=>setToggle(!toggle)}
+           />
+
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+
+
+
+
